f29d254df9501986a0d0b5d83785c91fdfab00dc
aldusd <aldusdanielfsm@gmail.com>
2023-06-27 19:35:53 -0300

    configures frontend docker

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 app
   .use(cors())
   .use(express.json())
-  .get('/health', (_, res) => res.status(200).send('Believe in yourself!!'));
+  .get('/health', (_, res) => res.status(200).send('Believe in yourself!'));
 
 export function init() {
   return Promise.resolve(app);
